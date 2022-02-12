78fb3dd53f6af9d4ce1055d98ef95a8f24a876da
Ro60t <99548546+Ro60t@users.noreply.github.com>
2022-02-12 19:26:36 +0300

    Update let's go out.py

--- let's go out.py
+++ let's go out.py
@@ -3,6 +3,6 @@ def letsGoOut():
   """
     Just a random simple function to ask Naima out
   """
-  print("Hello Naima, let's go on a caffe hopping date")
+  print("Hello Naima, let's go on a cafe-hopping date")
   
 letsGoOut()
